0bd6b8d0c27552cc5aec884a9fb859dd8f4e96ee
Tanish-anand <t01965454@gmail.com>
2024-09-10 23:09:35 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\final comfest\\Welcome-Login-Signup-Page-Flutter-master\\Welcome-Login-Signup-Page-Flutter-master" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=E:\\final comfest\\Welcome-Login-Signup-Page-Flutter-master\\Welcome-Login-Signup-Page-Flutter-master"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\final comfest\\Welcome-Login-Signup-Page-Flutter-master\\Welcome-Login-Signup-Page-Flutter-master\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\final comfest\\Welcome-Login-Signup-Page-Flutter-master\\Welcome-Login-Signup-Page-Flutter-master"
+  "FLUTTER_TARGET=E:\\final comfest\\Welcome-Login-Signup-Page-Flutter-master\\Welcome-Login-Signup-Page-Flutter-master\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\final comfest\\Welcome-Login-Signup-Page-Flutter-master\\Welcome-Login-Signup-Page-Flutter-master\\.dart_tool\\package_config.json"
+)
